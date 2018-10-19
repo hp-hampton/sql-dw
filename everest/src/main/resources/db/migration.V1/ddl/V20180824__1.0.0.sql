bc71db4b82b03701534a18c4da0f2052a890d0cc
hupeng <hupeng@youzan.com>
2018-10-19 15:27:45 +0800

    增加用户表

--- everest/src/main/resources/db/migration.V1/ddl/V20180824__1.0.0.sql
+++ everest/src/main/resources/db/migration.V1/ddl/V20180824__1.0.0.sql
@@ -25,14 +25,14 @@ CREATE TABLE client
   COMMENT '自动授予',
   enabled                TINYINT(1) DEFAULT '1'                                                                            NOT NULL,
   created_at             TIMESTAMP DEFAULT CURRENT_TIMESTAMP                                                               NOT NULL,
-  updated_at             TIMESTAMP DEFAULT '2000-01-01 00:00:00'                                                           NOT NULL,
+  updated_at             TIMESTAMP DEFAULT CURRENT_TIMESTAMP                                                               NOT NULL,
   CONSTRAINT client_client_id_uindex
   UNIQUE (client_id)
 )
   ENGINE = InnoDB;
 
 /**
- * 全局用户表
+ * 用户登陆表表
  */
 CREATE TABLE global_user
 (
@@ -41,8 +41,6 @@ CREATE TABLE global_user
     PRIMARY KEY,
   guid       VARCHAR(64)                                   NOT NULL
   COMMENT '用户全局 ID',
-  tenant_id  BIGINT                                        NOT NULL
-  COMMENT '租户 ID',
   username   VARCHAR(20)                                   NOT NULL
   COMMENT '用户名',
   password   VARCHAR(128)                                  NOT NULL
@@ -53,8 +51,28 @@ CREATE TABLE global_user
   expired    TINYINT(1) DEFAULT '0'                        NULL
   COMMENT '用户过期',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP           NOT NULL,
-  updated_at TIMESTAMP DEFAULT '2000-01-01 00:00:00'       NOT NULL,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP           NOT NULL,
   CONSTRAINT user_guid_uindex
   UNIQUE (guid)
 )
   ENGINE = InnoDB;
+
+CREATE TABLE user(
+  id         BIGINT AUTO_INCREMENT
+  COMMENT '用户 ID'
+    PRIMARY KEY,
+  guid       VARCHAR(64)                                   NOT NULL
+  COMMENT '用户全局 ID',
+  real_name  VARCHAR (10)                                  NULL
+  COMMENT '真实姓名',
+  identity_type VARCHAR(10)                                NULL
+  COMMENT '证件类型： 身份证， 军官证， 护照',
+  identity_no VARCHAR(20)                                  NULL
+  COMMENT '证件号码',
+  gender CHAR(1)                                           NULL
+  COMMENT '性别',
+  mobile_phone INT(11)                                     NOT NULL
+  COMMENT '手机号码',
+  register_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP        NOT NULL
+  COMMENT '注册时间',
+)
